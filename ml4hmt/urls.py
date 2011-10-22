527eb0c4229375a444a755221008302321a6109d
Christian Federmann <cfedermann@dfki.de>
2011-10-22 10:51:31 +0200

    Disabled signup view and added URL to program.

--- ml4hmt/urls.py
+++ ml4hmt/urls.py
@@ -8,15 +8,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
   url(r'^$', 'ml4hmt.views.home', name='home'),
   url(r'^(?P<page_id>call-for-papers)/$', 'ml4hmt.views.page', name='page'),
-  url(r'^signup/$', 'ml4hmt.views.signup', name='signup'),
-  url(r'^thank-you/$', 'ml4hmt.views.thank_you', name="thank-you"),
-    # Examples:
-    # url(r'^$', 'ml4hmt.views.home', name='home'),
-    # url(r'^ml4hmt/', include('ml4hmt.foo.urls')),
+  url(r'^(?P<page_id>program)/$', 'ml4hmt.views.page', name='page'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+  ###url(r'^signup/$', 'ml4hmt.views.signup', name='signup'),
+  ###url(r'^thank-you/$', 'ml4hmt.views.thank_you', name="thank-you"),
+  
+  # Uncomment the next line to enable the admin:
+  url(r'^admin/', include(admin.site.urls)),
 )
