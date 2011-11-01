5db763c4a4b47b59195b0e3d658d51f834b50695
Christian Federmann <cfedermann@dfki.de>
2011-11-01 15:54:16 +0100

    Updated urls.py to the new URL_PREFIX structure.

--- ml4hmt/urls.py
+++ ml4hmt/urls.py
@@ -5,19 +5,17 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-URL_PREFIX = 'ml4hmt/'
-
 urlpatterns = patterns('',
-  url(r'^{0}$'.format(URL_PREFIX),
+  url(r'^$',
     'ml4hmt.views.home', name='home'),
-  url(r'^{0}(?P<page_id>call-for-papers)/$'.format(URL_PREFIX),
+  url(r'^(?P<page_id>call-for-papers)/$',
     'ml4hmt.views.page', name='call-for-papers'),
-  url(r'^{0}(?P<page_id>program)/$'.format(URL_PREFIX),
+  url(r'^(?P<page_id>program)/$',
     'ml4hmt.views.page', name='program'),
-  url(r'^{0}signup/$'.format(URL_PREFIX),
+  url(r'^signup/$',
     'ml4hmt.views.signup', name='signup'),
-  url(r'^{0}thank-you/$'.format(URL_PREFIX),
+  url(r'^thank-you/$',
     'ml4hmt.views.thank_you', name="thank-you"),
-  url(r'^{0}admin/'.format(URL_PREFIX),
+  url(r'^admin/',
     include(admin.site.urls)),
 )
