bef3bf3e75d8a82ad6eb86a702c62c87ac845fbb
Christian Federmann <cfedermann@dfki.de>
2011-11-01 15:54:37 +0100

    Updated views to feature URL_PREFIX in local context for view methods.

--- ml4hmt/views.py
+++ ml4hmt/views.py
@@ -1,18 +1,22 @@
 # Title:  Django project for the ML4HMT 2011 workshop website
 # Author: Christian Federmann <cfedermann@dfki.de>
 
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from ml4hmt.workshop.forms import ParticipantForm
 from ml4hmt.workshop.models import Participant
+from ml4hmt.settings import URL_PREFIX
 
 def home(request):
-    return render_to_response('home.html')
+    context = {'URL_PREFIX': URL_PREFIX}
+    return render_to_response('home.html', context)
 
 def page(request, page_id):
     """Renders a page for the workshop."""
-    return render_to_response('{0}.html'.format(page_id))
+    context = {'URL_PREFIX': URL_PREFIX}
+    return render_to_response('{0}.html'.format(page_id), context)
 
 def signup(request):
     """Creates a signup form for interested participants."""
@@ -25,16 +29,16 @@ def signup(request):
               affiliation=cleaned_data['affiliation'])
             new_participant.save()
             uuid = new_participant.uuid
-            return HttpResponseRedirect('http://www.dfki.de/ml4hmt/thank-you/?uuid={0}'.format(uuid))
+            return HttpResponseRedirect(reverse('thank-you'))
     
     else:
         form = ParticipantForm()
     
-    context = {'form': form}
+    context = {'form': form, 'URL_PREFIX': URL_PREFIX}
     return render_to_response('participants/signup.html', context,
        context_instance=RequestContext(request))
 
 def thank_you(request):
-    uuid = request.GET.get('uuid')
-    context = {'message': 'We have saved your request, please note down your personal ID: <strong>{0}</strong>. We will contact you soon!'.format(uuid)}
+    context = {'message': 'Thanks for participating in the ML4HMT ' \
+      'workshop. See you soon!', 'URL_PREFIX': URL_PREFIX}
     return render_to_response('home.html', context) 
