ab6b399b182ba5090793356a49d08c8ac539971c
Christian Federmann <cfedermann@gmail.com>
2013-03-20 20:53:17 +0100

    Removed dynamic views for signup, participants and thank-you pages.

--- ml4hmt/views.py
+++ ml4hmt/views.py
@@ -16,8 +16,8 @@ def robots(request):
     _robots = ['User-agent: *']
     
     # The following URLs should NOT be crawled by bots, so we disallow access!
-    for url in ('signup', 'thank-you', 'participants', 'admin'):
-        _robots.append('Disallow: {0}/{1}/'.format(URL_PREFIX, url))
+    # for url in ('admin',):
+    #    _robots.append('Disallow: {0}/{1}/'.format(URL_PREFIX, url))
     
     return HttpResponse('\n'.join(_robots), mimetype='text/plain')
 
@@ -50,34 +50,3 @@ def markup_page(request, page_id):
     title = basename(page_id).capitalize()
     context = {'URL_PREFIX': URL_PREFIX, 'markup': markup, 'title': title}
     return render_to_response('markdown.html', context)
-
-def signup(request):
-    """Creates a signup form for interested participants."""
-    if request.method == "POST":
-        form = ParticipantForm(request.POST)
-        if form.is_valid():
-            cleaned_data = form.cleaned_data
-            new_participant = Participant(name=cleaned_data['name'],
-              email=cleaned_data['email'],
-              affiliation=cleaned_data['affiliation'])
-            new_participant.save()
-            _uuid = new_participant.uuid
-            return HttpResponseRedirect(reverse('thank-you'))
-    
-    else:
-        form = ParticipantForm()
-    
-    context = {'form': form, 'URL_PREFIX': URL_PREFIX}
-    return render_to_response('participants/signup.html', context,
-       context_instance=RequestContext(request))
-
-def thank_you(request):
-    context = {'message': 'Thanks for participating in the ML4HMT ' \
-      'workshop. See you soon!', 'URL_PREFIX': URL_PREFIX}
-    return render_to_response('home.html', context) 
-
-def participants(request):
-    """Renders a list of participants for the workshop."""
-    _participants = Participant.objects.all()
-    context = {'participants': _participants, 'URL_PREFIX': URL_PREFIX}
-    return render_to_response('participants/participants.html', context)
